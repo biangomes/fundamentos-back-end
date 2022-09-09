ccc860c930c0a60e0bedbfd147adfafce7f6a174
biangomes <biangomes0707@gmail.com>
2022-09-09 11:51:57 -0300

    Criando UnidadeServlet, porem nao ta trazendo os dados corretos

--- src/main/java/br/ufac/sgcm/UnidadeServlet.java
+++ src/main/java/br/ufac/sgcm/UnidadeServlet.java
@@ -1,6 +1,12 @@
 package br.ufac.sgcm;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.ufac.sgcm.dao.UnidadeDAO;
+import br.ufac.sgcm.model.Unidade;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,5 +16,20 @@ import jakarta.servlet.http.HttpServletResponse;
 public class UnidadeServlet extends HttpServlet {
     
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {}
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+
+        UnidadeDAO unidadeDao = new UnidadeDAO();
+        List<Unidade> unidades = new ArrayList<>();
+        Unidade unidade = new Unidade();
+
+        if (req.getParameter("id") != null) {
+                writer.println(unidade.getId() + " - " + unidade.getNome());
+        } else {
+            for (Unidade uni: unidades) {
+                writer.println(uni.getId() + " - " + uni.getNome() + " - " + uni.getEndereco());
+            }
+        }
+
+    }
 }
