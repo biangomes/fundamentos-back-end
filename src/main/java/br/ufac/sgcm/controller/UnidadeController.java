7b216c4d2fbdab10dc46927f9f04b494e85e8cc6
beatriz <biangomes0707@gmail.com>
2022-09-14 15:54:01 -0500

    UnidadeController finalizada

--- src/main/java/br/ufac/sgcm/controller/UnidadeController.java
+++ src/main/java/br/ufac/sgcm/controller/UnidadeController.java
@@ -3,35 +3,32 @@ package br.ufac.sgcm.controller;
 import java.sql.SQLException;
 import java.util.List;
 
-import br.ufac.sgcm.dao.ProfissionalDAO;
-import br.ufac.sgcm.model.Profissional;
+import br.ufac.sgcm.dao.UnidadeDAO;
+import br.ufac.sgcm.model.Unidade;
 
-public class ProfissionalController implements IController<Profissional> {
-    
-    private ProfissionalDAO dao;
+public class UnidadeController implements IController<Unidade>{
 
-    public ProfissionalController() {
-        dao = new ProfissionalDAO();
+    private UnidadeDAO dao;
+
+    public UnidadeController() {
+        dao = new UnidadeDAO();
     }
 
 
     @Override
-    public List<Profissional> get() {
+    public List<Unidade> get() {
         return dao.get();
     }
 
-    
+
     @Override
-    public Profissional get(Long id) {
+    public Unidade get(Long id) {
         return dao.get(id);
     }
 
 
-    /* (non-Javadoc)
-     * @see br.ufac.sgcm.controller.IController#save(java.lang.Object)
-     */
     @Override
-    public int save(Profissional objeto) throws SQLException {
+    public int save(Unidade objeto) throws SQLException {
 
         int registrosAfetados = 0;
 
@@ -42,6 +39,7 @@ public class ProfissionalController implements IController<Profissional> {
         }
 
         return registrosAfetados;
+
     }
 
 
@@ -49,4 +47,5 @@ public class ProfissionalController implements IController<Profissional> {
     public int delete(Long id) throws SQLException {
         return dao.delete(id);
     }
+    
 }
